01b5867672fbe40c92df0964287bbcbe30510cc8
Katja Levring <katj6233@stud.kea.dk>
2022-02-03 13:47:54 +0100

    se om branch virker

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#000000",
+      secondary: "#ffffff",
+      third: "#F6FF10",
+    },
+
+    fontFamily: {
+      oswald: "'Oswald', sans-serif;",
+      montserrat: "'Montserrat', sans-serif;",
+    },
+
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
