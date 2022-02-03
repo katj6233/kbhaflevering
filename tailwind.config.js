893553b2add2a876aed007603c4135319524d272
Katja Levring <katj6233@stud.kea.dk>
2022-02-03 17:41:33 +0100

    mobile view er done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@ module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     colors: {
+      /*sort*/
       primary: "#000000",
+      /*hvid*/
       secondary: "#ffffff",
+      /*gul*/
       third: "#F6FF10",
+      /*blå*/
       fourth: "#0031FF",
+      /*grå*/
+      fifth: "#2A2B2E",
     },
 
     fontFamily: {
